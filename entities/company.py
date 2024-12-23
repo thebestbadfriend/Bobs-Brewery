604facde3ca846eec744c0f6a96c7dfaf464e779
thebestbadfriend <51896268+thebestbadfriend@users.noreply.github.com>
2024-12-23 16:01:33 -0600

    started on company.py

--- entities/company.py
+++ entities/company.py
@@ -1,6 +1,22 @@
-class Company(object):
-    def __init__(self):
-        pass
+from dal import BreweryDBAccessor
 
-    address_ids = []
-    people_ids = []
+
+class Company(object):
+    bda = BreweryDBAccessor()
+
+    id = 0
+    contact_id = 0
+    name = ""
+
+    addresses = []
+    people = []
+
+    def __init__(self, id, contact_id, name, addresses, people):
+        self.id = id
+        self.contact_id = contact_id
+        self.name = name
+        self.addresses = addresses
+        self.people = people
+
+    def get_people(self):
+        people = self.bda.execute_db_command(f"select * from contacts where id = {self.id}")
