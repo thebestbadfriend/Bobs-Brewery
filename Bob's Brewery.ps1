f025627e817e4b90858b03862b21f1e83177e993
brewerstruss <brewerstrusstech@gmail.com>
2025-01-29 16:05:10 -0600

    compiling is garbage. wrapping it in a ps1 script

---
+++ Bob's Brewery.ps1
@@ -0,0 +1,5 @@
+# Execute the bob's brewery program without all the compilation hassle
+$python_path = ".\.venv3.12\Scripts\python.exe"
+$bb_entry_point = ".\main.py"
+
+& $python_path $bb_entry_point
