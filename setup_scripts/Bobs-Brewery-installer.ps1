00512b272459f13a5dd942116de53dc835ce329f
brewerstruss <brewerstrusstech@gmail.com>
2025-03-13 10:00:53 -0500

    python 3 download working

--- setup_scripts/Bobs-Brewery-installer.ps1
+++ setup_scripts/Bobs-Brewery-installer.ps1
@@ -1,11 +1,19 @@
 $scriptPath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptPath
 
-$python3Check = Get-Command python -all 2>$null | Where-Object {$_.Version -Like "3.*"} 2>$null
+$python3Check = Get-Command asdf -all 2>$null | Where-Object {$_.Version -Like "3.*"} 2>$null
+
 
 if ($python3Check) {
     "Python 3 is already installed and in the path"
 }
 else {
-    "Python 3 is either not installed or not in the path"
+    "installing python"
+    Install-Python
+    "python installed"
+}
+
+
+function Install-Python {
+    Invoke-WebRequest https://www.python.org/ftp/python/3.13.2/python-3.13.2-amd64.exe -OutFile C:\Users\bt-se\Downloads\my-py-installer.exe
 }
