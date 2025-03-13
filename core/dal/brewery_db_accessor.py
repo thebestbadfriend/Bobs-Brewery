cc3d7ebfb9544cd944690f0c1a9e9172ed68616e
brewerstruss <brewerstrusstech@gmail.com>
2025-03-13 13:55:49 -0500

    tinkering with remote db access

--- core/dal/brewery_db_accessor.py
+++ core/dal/brewery_db_accessor.py
@@ -10,7 +10,8 @@ class BreweryDBAccessor:
     # Configuration - update these variables
     pg_user = cv.DB_USER
     pg_password = cv.COMMON_PASS
-    pg_host = cv.DB_HOST
+    # pg_host = cv.DB_HOST
+    pg_host = "71.31.104.78"
     pg_port = '5432'
     db_name = 'bb_contacts'
     dump_directory = r'C:\Toolbox\Coding\Brewers Truss\Bobs-Brewery\dal\dumps'
--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from core.ui import WindowBuilder as wb
 
 def main():
     # root = wb.create_main_window()
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    print(BASE_DIR)
-    root = wb.create_widget_from_file(rf"{BASE_DIR}\core\ui\ui_elements\windows\main_window.json")
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    root = wb.create_widget_from_file(rf"{base_dir}\core\ui\ui_elements\windows\main_window.json")
     print('window should display in <1s')
     root.mainloop()
 
