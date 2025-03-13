4f3c2c4ae4aa467080b9d6f6556d58f9908b91ba
brewerstruss <brewerstrusstech@gmail.com>
2025-03-13 08:13:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from core.ui import WindowBuilder as wb
 def main():
     # root = wb.create_main_window()
     root = wb.create_widget_from_file(r"core\ui\ui_elements\windows\main_window.json")
+    print('window should display in <1s')
     root.mainloop()
+
 
 
 main()
