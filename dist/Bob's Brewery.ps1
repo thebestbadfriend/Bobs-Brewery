2e67408e03d2e3e72fde75e2cc731c0525f99adc
brewerstruss <brewerstrusstech@gmail.com>
2025-03-17 10:48:10 -0500

    removed an unnecessary file

--- dist/Bob's Brewery.ps1
+++
@@ -1,7 +0,0 @@
-# Execute the bob's brewery program without all the compilation hassle
-$scriptPath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptPath
-
-$BBEntryPoint = "main.py"
-
-python "$($dir)\\$($BBEntryPoint)"
--- dist/main.py
+++ dist/main.py
@@ -6,6 +6,8 @@ from core.ui import WindowBuilder as wb
 def main():
     print('If this window is open, then the program is running.')
     print("It can take a few minutes to load. I'm working on improving that.")
+    print()
+    print("Note: closing this window will close Bob's Brewery.")
 
     root = wb.create_widget_from_file(rf"{cfg.source_root}\core\ui\ui_elements\windows\main_window.json")
 
