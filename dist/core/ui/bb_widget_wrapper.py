85695ec65d974a17b523c04ac805c9fc87d58ea0
brewerstruss <brewerstrusstech@gmail.com>
2025-04-17 09:03:32 -0500

    Update bb_widget_wrapper.py

--- dist/core/ui/bb_widget_wrapper.py
+++ dist/core/ui/bb_widget_wrapper.py
@@ -8,15 +8,30 @@ class BBWidgetWrapper:
     '''
     widget = None
 
-    json = {}
+    full_dict = {}
     name = ''
     parent = None
     properties = {}
     pack_properties = {}
     children = []
 
-    def __init__(self):
-        pass
+    def __init__(self, full_dict, parent=None):
+        self.parent = parent
+        self.full_dict = full_dict
+        self.create_children()
 
     def create_children(self):
+        child_list = self.full_dict.get('children', [])
+        if child_list:
+            for child_dict in child_list:
+                self.create_child(child_dict)
+
+    def create_child(self, child_dict):
+        child = BBWidgetWrapper(full_dict=child_dict, parent=self)
+        self.children.append(child)
+
+    def get_or_import_library(self, library_name):
+        pass
+
+    def pack(self):
         pass
