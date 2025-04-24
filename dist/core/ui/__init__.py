008faf626ae2be3d9cd57140f2382159b50a1f60
brewerstruss <brewerstrusstech@gmail.com>
2025-04-24 14:57:27 -0500

    switched main window load to call WindowWrapper

--- dist/core/ui/__init__.py
+++ dist/core/ui/__init__.py
@@ -1,4 +1,4 @@
 from .bb_treeview import BBTreeview
 from .notebook_builder import NotebookBuilder
-from .window_builder import WindowBuilder
+from .window_wrapper import WindowWrapper
 from .widget_wrapper import WidgetWrapper
--- dist/main.py
+++ dist/main.py
@@ -1,12 +1,12 @@
 # https://www.pythonguis.com/tutorials/create-gui-tkinter/
 import config as cfg
-from core.ui import WindowBuilder as wb
+from core.ui import WindowWrapper
 
 
 def main():
     print("Note: closing this window will close Bob's Brewery.")
 
-    root = wb.create_widget_from_file(rf"{cfg.source_root}\core\ui\ui_elements\windows\main_window.json")
+    root = WindowWrapper.load_window_from_file(rf"{cfg.source_root}\core\ui\ui_elements\windows\main_window.json")
     root.window.mainloop()
 
 
