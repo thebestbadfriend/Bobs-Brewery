eb6a6139da6140ebeeac6962d94fcb02f3fc5c85
brewerstruss <brewerstrusstech@gmail.com>
2025-04-28 08:06:55 -0500

    notebook loads but does not populate

--- dist/core/ui/widget_wrapper.py
+++ dist/core/ui/widget_wrapper.py
@@ -40,11 +40,8 @@ class WidgetWrapper:
             return self.load_widget_from_file(file, self.window, self.parent)
         else:
             widget_library_name = self.full_dict.get("library")
-            print(rf'widget library name is : {widget_library_name}')
             widget_library = utilities.get_or_import(widget_library_name)
             widget_type = self.full_dict.get("type")
-            print(rf'widget type is : {widget_type}')
-            print('---')
             properties = utilities.get_objectified_dict(self.full_dict.get("properties", {}))
 
             widget_class = getattr(widget_library, widget_type)
@@ -54,7 +51,6 @@ class WidgetWrapper:
                 widget = widget_class(self.parent.widget, **properties)
 
             return widget
-
 
     def create_children(self):
         child_list = self.full_dict.get('children', [])
@@ -66,26 +62,6 @@ class WidgetWrapper:
         child = WidgetWrapper(full_dict=child_dict, window=self.window, parent=self)
         self.children.append(child)
 
-    # TODO
-    # Move check_widget_exists from widget_wrapper, whose concern it is not, into window_wrapper (or maybe
-    # registry_stack). Does not need to be able to search other windows either. If there's a need later, I can explore
-    # that.
-    def check_widget_exists(self, widget_name, current_window_only=True):
-        widget_exists = False
-        searchable_windows = []
-
-        if current_window_only:
-            searchable_windows.append(self.window)
-        else:
-            for window in WidgetWrapper.global_widget_registry.keys():
-                searchable_windows.append(window)
-
-        for window in searchable_windows:
-            if widget_name in WidgetWrapper.global_widget_registry[window].keys():
-                widget_exists = True
-
-        return widget_exists
-
     def pack(self):
         if self.parent.full_dict.get('type', '') == 'Notebook':
             notebook = self.parent.widget
@@ -95,19 +71,25 @@ class WidgetWrapper:
             pack_props = utilities.get_objectified_dict(self.full_dict.get("pack_properties", {}))
             self.widget.pack(**pack_props)
 
+    def check_widget_exists(self, widget_name):
+        print(widget_name)
+        print(widget_name in self.window.widget_registry.keys())
+        return widget_name in self.window.widget_registry.keys()
+
     def pre_widget_creation_tasks(self):
         properties = self.full_dict.get("properties", {})
+        print(self.name)
         if properties.get('command',''):
             command_path = '.'.join(properties["command"].split('.')[:-1])
             func_name = properties["command"].split('.')[-1]
 
-            print(command_path)
-            module = utilities.get_or_import(command_path)
-            if module:
-                func = getattr(module, func_name)
-                properties["command"] = func
+            if self.check_widget_exists(command_path):
+                module = self.window.widget_registry[command_path].widget
             else:
-                print(rf'could not find module: {command_path}')
+                module = utilities.get_or_import(command_path)
+
+            func = getattr(module, func_name)
+            properties["command"] = func
 
     def post_widget_creation_tasks(self):
         if self.full_dict.get('type', '') == "Scrollbar":
@@ -116,8 +98,8 @@ class WidgetWrapper:
         binds = self.full_dict.get("binds", {})
         if binds:
             for event_name, event_config in binds.items():
-                event_config['module'] = globals().get(event_config["module"])
-                func = getattr(event_config['module'], event_config['function'])
+                event_config['class'] = utilities.get_or_import(event_config["module"], event_config['class'])
+                func = getattr(event_config['class'], event_config['function'])
                 args = event_config['args']
                 args['widget'] = self.widget
 
@@ -126,11 +108,12 @@ class WidgetWrapper:
         populate_function = self.full_dict.get("populate_function",'')
         if populate_function:
             module_name = populate_function["module"]
+            class_name = populate_function["class"]
             func_name = populate_function["function_name"]
 
-            module = utilities.get_or_import(module_name)
-            if module:
-                func = getattr(module, func_name)
+            event_class = utilities.get_or_import(module_name, class_name)
+            if event_class:
+                func = getattr(event_class, func_name)
                 func(self.widget)
 
     @staticmethod
