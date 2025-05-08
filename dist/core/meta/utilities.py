48c1307a6dc80d6e7755c1177b02ca77cb442d61
brewerstruss <brewerstrusstech@gmail.com>
2025-05-08 09:59:16 -0500

    setting up function resolution from string

--- dist/core/meta/utilities.py
+++ dist/core/meta/utilities.py
@@ -11,16 +11,27 @@ def get_or_import(module_path, class_name=''):
 
 
 def resolve_object_reference_from_string(string_value, context):
-    if not string_value.startswith('@@'):
+    if not string_value.startswith('@obj@'):
         return string_value
 
-    object_path = string_value[2:].split('.')
+    object_path = string_value[5:].split('.')
     obj = context
 
     for attr in object_path:
         obj = getattr(obj, attr)
 
     return obj
+
+
+def resolve_function_reference_from_string(string_value, context):
+    if not string_value.startswith('@func@'):
+        return string_value
+
+    func = string_value[6:].split('.')
+
+    # all the things
+
+    return func
 
 
 def get_objectified_dict(dictionary):
--- dist/modules/contact_management/event_handlers.py
+++ dist/modules/contact_management/event_handlers.py
@@ -5,6 +5,9 @@ from . import dal
 def btn_add_contacts_clicked(window):
     window.create_modal_window_from_file(rf'{cfg.source_root}\core\ui\ui_elements\windows\add_contacts_window.json', window)
 
+def btn_update_contact_clicked(window):
+    window.create_modal_window_from_file(rf'{cfg.source_root}\core\ui\ui_elements\windows\update_contact_window.json', window)
+
 
 def btn_add_company_clicked(window):
     company_name = window.widget_registry.get('txt_company_name').widget.get()
